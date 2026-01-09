86ddc1c841e3ebbcbb6fc3cee7ab9fedcdbfa782
s90246 <s90246@bht-berlin.de>
2026-01-09 19:51:41 +0100

    added streamlit app

---
+++ app.py
@@ -0,0 +1,92 @@
+import streamlit as st 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+st.title("Mental Health and Social Media")
+st.subheader("Übersicht")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv('Datasets/ihr_datensatz_cleaned.csv')
+
+
+def main():
+    df = load_data()
+    st.write(f'Datensatz enthält **{len(df)} Patienten** und **{len(df.columns)} Features**')
+    st.dataframe(df.head(10))
+
+    st.subheader('Statistiken')
+    st.write(df.describe())
+
+    st.header('Visualisierungen')
+
+    st.subheader('Verteilung der täglichen Bildschirmzeit')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.hist(df['Daily_Screen_Time(hrs)'], bins=30, edgecolor='black', color='skyblue')
+    ax.set_xlabel('Daily Screen Time(hrs)', fontsize=12)
+    ax.set_ylabel('Häufigkeit', fontsize=12)
+    ax.set_title('Verteilung der Daily Screen Time')
+    ax.grid(True, alpha=0.3)
+    st.pyplot(fig)
+
+
+    st.subheader('Häufigkeit von Tagen ohne Social Media')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.boxplot(y=df['Days_Without_Social_Media'])
+    plt.title('Boxplot von Days_Without_Social_Media')
+    st.pyplot(fig)
+
+
+    st.subheader('Übersicht aller korrelierenden Features')
+    numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
+    correlation_matrix = df[numeric_cols].corr()
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', center=0, 
+                square=True, linewidths=1, fmt='.2f')
+    plt.title('Korrelationsmatrix')
+    st.pyplot(fig)
+
+    st.subheader('Beziehung zwischen der täglichen Bildschirmzeit und dem Stress level')
+    numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
+    fig, ax = plt.subplots(figsize=(10, 6))
+    plt.scatter(df["Daily_Screen_Time(hrs)"], df["Stress_Level(1-10)"], alpha=0.5)
+    plt.xlabel('[Daily_Screen_Time(hrs)]')
+    plt.ylabel('[Stress_Level(1-10)]')
+    st.pyplot(fig)
+
+    st.subheader('Beziehung zwischen der täglichen Bildschirmzeit und der Schlafqualität')
+    numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
+    fig, ax = plt.subplots(figsize=(10, 6))
+    plt.scatter(df["Daily_Screen_Time(hrs)"], df["Sleep_Quality(1-10)"], alpha=0.5)
+    plt.title('Beziehung zwischen [Daily_Screen_Time(hrs)] und [Sleep_Quality(1-10)]')
+    plt.xlabel('[Daily_Screen_Time(hrs)]')
+    plt.ylabel('[Sleep_Quality(1-10)]')
+    st.pyplot(fig)
+
+
+    st.title('Interaktiver Filter 🎛️')
+    daily_screen_time_filter = st.slider(
+        'Bildschirmzeit auswählen:',
+        min_value=int(df['Daily_Screen_Time(hrs)'].min()),
+        max_value=int(df['Daily_Screen_Time(hrs)'].max()),
+        value=(0, 10)  # Standard-Bereich
+    )
+
+    # Daten filtern
+    filtered_df = df[
+        (df['Daily_Screen_Time(hrs)'] >= daily_screen_time_filter[0]) & 
+        (df['Daily_Screen_Time(hrs)'] <= daily_screen_time_filter[1])
+    ]
+
+    # Ergebnis anzeigen
+    st.write(f'Zeige **{len(filtered_df)}** von {len(df)} Personen')
+    st.write(f'Bildschirmzeit zwischen {daily_screen_time_filter[0]} und {daily_screen_time_filter[1]} Stunden')
+    st.dataframe(filtered_df)
+
+    st.markdown("---")
+    st.write("Datenquelle: Social Median and Mental Health Balance Dataset")
+
+if __name__ == "__main__":
+    main()
